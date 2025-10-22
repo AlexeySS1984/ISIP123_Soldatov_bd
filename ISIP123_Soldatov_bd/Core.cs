1a13c400a891dc5782128c0106c4a61ca4e128f0
Алексей Солдатов <alexey.soldatov.1997@gmail.com>
2025-10-22 23:24:48 +0300

    я устал

---
+++ ISIP123_Soldatov_bd/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP123_Soldatov_bd
+{
+    internal class Core
+    {
+        public static AutoServiceNoNeuroEntities Context = new AutoServiceNoNeuroEntities();
+    }
+}
