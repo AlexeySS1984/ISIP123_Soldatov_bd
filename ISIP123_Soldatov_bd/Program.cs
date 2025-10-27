5c8b73366a3025bce6edbe4890f3a484cdac15b4
AlexeySS1984 <alexey.soldatov.1997@gmail.com>
2025-10-27 13:55:21 +0300

    commit1

--- ISIP123_Soldatov_bd/Program.cs
+++ ISIP123_Soldatov_bd/Program.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// Регистрация
+// 1. Создать экземпляр класса пользователя из БД
+// 2. Попросить пользователя заполнить экземпляр данными
+// 3. Проверить не занят ли логин(почта/телефон) введённый пользователем пользователя
+// 4. Если все прошло успешно добавить пользователя и войти в аккаунт\вернутся в меню
+// 5. 
 namespace ISIP123_Soldatov_bd
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
